06c889425a1ae43e46d783745a7e6f10a27b29d6
Validmannuel <159830493+Validmannuel@users.noreply.github.com>
2024-02-12 12:41:54 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("EaglesFly");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Eagles");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Fly");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
